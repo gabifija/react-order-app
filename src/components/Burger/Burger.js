b7afdaa8ad5d21d90a17bb6ef7bc49b333551331
Gabi <gabriela@assembla.com>
2018-01-02 10:28:01 +0100

    Outputting Burger Ingredients Dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,17 @@ import classes from './Burger.css';
 import BurgerIngridient from './BurgerIngridient/BurgerIngridient';
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingridients)
+    .map(igKey => {
+      return [...Array(props.ingridients[igKey])].map((_, i) => {
+        return <BurgerIngridient key={igKey + i} type={igKey} />;
+      });
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngridient type="bread-top" />
-      <BurgerIngridient type="cheese" />
-      <BurgerIngridient type="meat" />
+      {transformedIngredients}
       <BurgerIngridient type="bread-bottom" />
     </div>
   );
