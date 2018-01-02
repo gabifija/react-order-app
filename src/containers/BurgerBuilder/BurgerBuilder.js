92ba351805d91f31a50e37c58dcf145d4c9f66ea
Gabi <gabriela@assembla.com>
2018-01-02 11:21:54 +0100

    Removing Ingredients Safetly

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,16 +36,37 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredientHandler = (type) => {
-
+    const oldCount = this.state.ingredients[type];
+    if (oldCount <= 0) { return; }
+    const updatedCount = oldCount - 1;
+    const updatedIngrediets = {
+      ...this.state.ingredients
+    };
+    updatedIngrediets[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngrediets });
   }
 
   render () {
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
+
+    // {salad: true, meat: false, ...}
+
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
-          ingredientAdded={this.addIngredientHandler} />
-        <div>Build Controls</div>
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredientHandler}
+          disabled={disabledInfo} />
       </Aux>
     );
   }
